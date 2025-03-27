cf1bc23841010a55beb955204b9ff2ae57a8c676
Angel Lengua <renelengua26@gmail.com>
2025-03-26 19:31:37 -0500

    Guardado de token JWT en localStorage tras autenticación exitosa

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,11 +2,31 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const api = axios.create({
+//Ahora, en cualquier parte del frontend donde necesites hacer peticiones,
+// puedes importar api en lugar de repetir la URL base en cada solicitud.
+export const api = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export default api;
+//Agregar una función para obtener el perfil del usuario
+export const getProfile = async () => {
+	const token = localStorage.getItem("token"); // Obtiene el token almacenado
+  
+	if (!token) {
+	  throw new Error("No hay token disponible");
+	}
+  
+	try {
+	  const response = await axios.get(`${API_URL}/users/profile`, {
+		headers: {
+		  Authorization: `Bearer ${token}`, // Enviar token en la cabecera
+		},
+	  });
+	  return response.data; // Retorna los datos del usuario
+	} catch (error: any) {
+	  throw new Error(error.response?.data?.error || "Error obteniendo perfil");
+	}
+  };
